de4da0bfcb7b3d5226063ccdc2efe879e2e8ea27
Mathias <mnbaltzersen@gmail.com>
2024-06-26 15:22:30 +0200

    Minor clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import ESRSSelector from "@/components/question/esrs-selector";
-import { handleESRSDatapoint } from "@/server/actions/ai/open-ai";
-const RootPage = () => {
-  return (
-    <div className="mx-auto">
-      {/* <ESRSSelector gippity={handleESRSDatapoint}></ESRSSelector> */}
-    </div>
-  );
-};
-export default RootPage;
+import { redirect } from "next/navigation";
+
+export default function RootPage() {
+  redirect("/home");
+
+  return <></>;
+}
