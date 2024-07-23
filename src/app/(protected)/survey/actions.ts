0fff5b09044fbde0fde8ae0af6490194d5f95099
Mathias <mnbaltzersen@gmail.com>
2024-07-23 17:14:31 +0200

    rename survey

--- src/app/(protected)/survey/actions.ts
+++ src/app/(protected)/survey/actions.ts
@@ -4,6 +4,7 @@ import { api } from "@/trpc/server";
 import { revalidatePath } from "next/cache";
 
 export const handleArchiveSurvey = async (surveyId: number) => {
+  console.log("Icalled this", surveyId);
   await api.survey.archiveById({ id: surveyId });
   revalidatePath(`/(protected)/survey`, "page");
   // TODO: Add Toaster here
