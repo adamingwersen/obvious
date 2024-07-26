395b7376ab9567dce63d7248c5b3273028f3e858
ellonde <mnbaltzersen@gmail.com>
2024-07-26 14:02:27 +0200

    Loading (#58)

---
+++ src/app/(protected)/survey/[surveyUuid]/loading.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Loading() {
+  return (
+    <div className=" flex h-full items-center justify-center gap-6">
+      <div className="h-12 w-12 animate-spin rounded-full border-t-4 border-nightsky-700"></div>
+    </div>
+  );
+}
